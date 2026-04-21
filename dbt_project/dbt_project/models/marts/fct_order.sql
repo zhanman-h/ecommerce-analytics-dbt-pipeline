b1056d7b1d8d63389e9afa203ac3d83664c23d50
Xtrazman <9274139+zhanman-h@users.noreply.github.com>
2026-04-21 13:47:51 -0700

    add marts layers: dim_product and fct_order

---
+++ dbt_project/dbt_project/models/marts/fct_order.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+SELECT 
+    order_id, 
+    customer_id, 
+    product_id, 
+    order_date, 
+    quantity, 
+    price, 
+    quantity * price AS total_amount,
+    order_status
+FROM {{ ref('int_ecommerce_sales') }}
